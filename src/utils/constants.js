3addd47c228ab67742037cf0ca51cacc09b31ebf
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-12-05 22:27:38 +0530

    update base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+  location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://devtinder-backend-4vck.onrender.com";
